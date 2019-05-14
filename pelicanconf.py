ad48f7a421e0094037ad3881fd62a38f0feab0cf
Danny Burnett <danny.burnett@brokertech.co.uk>
2019-05-14 17:06:05 +0100

    Updates

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,10 +21,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (("Pelican", "http://getpelican.com/"),
-         ("Python.org", "http://python.org/"),
-         ("Jinja2", "http://jinja.pocoo.org/"),
-         ("You can modify those links in your config file", "#"),)
+LINKS = ()
 
 # Social widget
 SOCIAL = (("You can add links in your config file", "#"),
